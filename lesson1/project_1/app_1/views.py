1e333f6f225776bd5cb9d45e88840992c547c791
Dima-Gomel <metalland2015@yandex.ru>
2025-01-05 23:45:26 +0300

    lesson1 - второй коммит

--- lesson1/project_1/app_1/views.py
+++ lesson1/project_1/app_1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def url1(reqest):
+    return HttpResponse("ответ 1")
+
+def url2(reqest):
+    return HttpResponse("ответ 2")
+
+def url3(reqest):
+    return HttpResponse("ответ 3")
