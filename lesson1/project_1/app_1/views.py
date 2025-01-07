53669615601d49d376388c87baffb0de179f54b1
Dima-Gomel <metalland2015@yandex.ru>
2025-01-07 15:30:41 +0300

    lesson1 - третий коммит

--- lesson1/project_1/app_1/views.py
+++ lesson1/project_1/app_1/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def url1(reqest):
+def url1(request):
     return HttpResponse("ответ 1")
 
-def url2(reqest):
+
+def url2(request):
     return HttpResponse("ответ 2")
 
-def url3(reqest):
-    return HttpResponse("ответ 3")
+
+def url3(request):
+    return HttpResponse(request)
